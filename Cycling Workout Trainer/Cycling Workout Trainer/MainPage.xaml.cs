9919f2c1174b64994697382144d267d1c6692902
Evgeny 'Riketta' Chugaev <riketta@outlook.com>
2022-04-11 21:28:20 +0300

    Add project files.

---
+++ Cycling Workout Trainer/Cycling Workout Trainer/MainPage.xaml.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
+using System.Reflection;
+using System.IO;
+using Xamarin.Essentials;
+
+namespace Cycling_Workout_Trainer
+{
+    public partial class MainPage : ContentPage
+    {
+        static readonly string TimerFormat = "mm:ss";
+        static readonly TimeSpan Second = new TimeSpan(0, 0, 1);
+        static readonly string metronome1Path = "Resources.metronome1.mp3";
+        static readonly string metronome2Path = "Resources.metronome2.mp3";
+
+        StackLayout layout = new StackLayout();
+        Label labelTotalProgress = new Label() { HorizontalTextAlignment = TextAlignment.Center, FontSize = 32 };
+        Label labelCurrentRPM = new Label() { HorizontalTextAlignment = TextAlignment.Center, FontSize = 40, Padding = new Thickness(0, 30, 0, 5) };
+        Label labelCurrentTimer = new Label() { HorizontalTextAlignment = TextAlignment.Center, FontSize = 60 };
+        Label labelCurrentDescription = new Label() { HorizontalTextAlignment = TextAlignment.Center, FontSize = 28 };
+        Label labelNextData = new Label() { HorizontalTextAlignment = TextAlignment.End, FontSize = 24, Padding = new Thickness(0, 25, 20, 0) };
+        Label labelNextDescription = new Label() { HorizontalTextAlignment = TextAlignment.End, FontSize = 24, Padding = new Thickness(0, 0, 20, 20) };
+        Button buttonNextStep = new Button() { Text = "Next Step" };
+        Picker trainingPicker = new Picker() { Title = "Trainings", VerticalOptions = LayoutOptions.CenterAndExpand };
+        Label labelStrobe = new Label() { Text = "Strobe", HorizontalTextAlignment = TextAlignment.Start, FontSize = 22 };
+        Label labelMetronome = new Label() { Text = "Metronome", HorizontalTextAlignment = TextAlignment.Start, FontSize = 22 };
+        CheckBox checkBoxStrobe = new CheckBox();
+        CheckBox checkBoxMetronome = new CheckBox();
+
+        bool strobeState = false;
+        bool metronomeState = false;
+        ISimpleAudioPlayer metronome1 = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+        ISimpleAudioPlayer metronome2 = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+
+        List<Training> trainingList = new List<Training>();
+        Training currentTraining = null;
+        TimeSpan stepStopwatch = TimeSpan.Zero;
+        TimeSpan trainingStopwatch = TimeSpan.Zero;
+        Timer rpmTimer = new Timer();
+
+        public MainPage()
+        {
+            //UIApplication.SharedApplication.IdleTimerDisabled = true;
+            DeviceDisplay.KeepScreenOn = true;
+
+            InitializeComponent();
+            BindingContext = this;
+
+#if DEBUG
+            /*
+            labelTotalProgress.Text = "Total: 42:00  -  Now: 62%";
+            labelCurrentRPM.Text = "85 RPM";
+            labelCurrentTimer.Text = "02:16";
+            labelCurrentDescription.Text = "Интенсивно (3-3)";
+            labelNextData.Text = "Next: 03:20, 60 RPM";
+            labelNextDescription.Text = "Режим труда";
+            
+            rpmTimer.Interval = 60 * 1000 / 80 * 2; // 60 seconds / 2 * RPM
+            */
+
+            trainingList.Add(Training.Debug);
+#endif
+            trainingList.Add(Training.Default);
+            trainingList.Add(Training.DefaultSmall);
+
+            foreach (var training in trainingList)
+                trainingPicker.Items.Add(training.Name);
+
+
+            rpmTimer.Elapsed += RpmTimer_Elapsed;
+            buttonNextStep.Clicked += ButtonNextStep_Clicked;
+            trainingPicker.SelectedIndexChanged += TrainingPicker_SelectedIndexChanged;
+
+            Grid grid = new Grid
+            {
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = GridLength.Auto },
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = GridLength.Auto },
+                    new ColumnDefinition { Width = GridLength.Auto },
+                    new ColumnDefinition { Width = GridLength.Auto },
+                    new ColumnDefinition { Width = GridLength.Auto },
+                }
+            };
+
+            grid.Children.Add(checkBoxStrobe, 0, 0);
+            grid.Children.Add(labelStrobe, 1, 0);
+            grid.Children.Add(checkBoxMetronome, 2, 0);
+            grid.Children.Add(labelMetronome, 3, 0);
+
+            layout.Padding = new Thickness(10, 10, 10, 5);
+            layout.Children.Add(trainingPicker);
+            layout.Children.Add(labelTotalProgress);
+            layout.Children.Add(labelCurrentRPM);
+            layout.Children.Add(labelCurrentTimer);
+            layout.Children.Add(labelCurrentDescription);
+            layout.Children.Add(labelNextData);
+            layout.Children.Add(labelNextDescription);
+            layout.Children.Add(buttonNextStep);
+            layout.Children.Add(grid);
+            
+            Content = layout;
+
+
+
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+            string resource1Path = nameof(Cycling_Workout_Trainer) + "." + metronome1Path;
+            string resource2Path = nameof(Cycling_Workout_Trainer) + "." + metronome2Path;
+            //foreach (var res in assembly.GetManifestResourceNames())
+            //    System.Diagnostics.Debug.WriteLine("Found resource: " + res);
+
+            Stream stream1 = assembly.GetManifestResourceStream(resource1Path);
+            Stream stream2 = assembly.GetManifestResourceStream(resource2Path);
+            metronome1.Load(stream1);
+            metronome2.Load(stream2);
+        }
+
+        private void StartTraining(string name)
+        {
+            currentTraining = trainingList.FirstOrDefault(t => t.Name == name);
+            rpmTimer.Stop();
+
+            stepStopwatch = TimeSpan.Zero;
+            trainingStopwatch = TimeSpan.Zero;
+
+            currentTraining.NextStepEvent += CurrentTraining_NextStepEvent;
+            currentTraining.TickEvent += CurrentTraining_TickEvent;
+            TrainingStep firstStep = currentTraining.Start();
+        }
+
+        private void UpdateNewStepData(TrainingStep step, TrainingStep nextStep)
+        {
+            rpmTimer.Interval = 60 * 1000 / (2 * step.RPM);
+            rpmTimer.Stop();
+            rpmTimer.Start();
+
+            stepStopwatch = TimeSpan.Zero + new TimeSpan(0, 0, step.Seconds);
+
+            labelCurrentRPM.Text = string.Format("{0} RPM", step.RPM);
+            //labelCurrentTimer.Text = string.Format("{0}", SecondsToTimerText(step.Seconds));
+            labelCurrentDescription.Text = step.Description;
+
+            if (nextStep != null)
+            {
+                labelNextData.Text = string.Format("Next: {0} - {1} RPM", SecondsToTimerText(nextStep.Seconds), nextStep.RPM);
+                labelNextDescription.Text = nextStep.Description;
+            }
+            else
+            {
+                labelNextData.Text = "Finish!";
+                labelNextDescription.Text = "";
+            }
+        }
+
+        private void UpdateProgress(int remainingStepSeconds)
+        {
+            labelCurrentTimer.Text = string.Format("{0}", SecondsToTimerText(Convert.ToInt32(stepStopwatch.TotalSeconds)));
+            labelTotalProgress.Text = string.Format(@"{0}\{1} - Step {2}\{3}",
+                SecondsToTimerText(Convert.ToInt32(trainingStopwatch.TotalSeconds)), SecondsToTimerText(currentTraining.TotalTrainingTime), currentTraining.CurrentStep, currentTraining.TotalSteps);
+
+            trainingStopwatch += Second;
+            stepStopwatch -= Second;
+        }
+
+        private string SecondsToTimerText(int seconds)
+        {
+            return new DateTime().AddSeconds(seconds).ToString(TimerFormat);
+        }
+
+        private void CurrentTraining_NextStepEvent(object sender, NextStepEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                UpdateNewStepData(e.NewStep, e.NextStep);
+            });
+        }
+
+        private void CurrentTraining_TickEvent(object sender, TickEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                UpdateProgress(e.RemainingStepSeconds);
+            });
+        }
+
+        private void RpmTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                if (checkBoxStrobe.IsChecked)
+                {
+                    BackgroundColor = strobeState ? Color.Pink : Color.LightBlue;
+                    layout.BackgroundColor = strobeState ? Color.Pink : Color.LightBlue;
+
+                    strobeState = !strobeState;
+                }
+
+                if (checkBoxMetronome.IsChecked)
+                {
+                    if (metronomeState)
+                        metronome1.Play();
+                    else
+                        metronome2.Play();
+
+                    metronomeState = !metronomeState;
+                }
+            });
+        }
+
+        private void TrainingPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (currentTraining != null)
+                currentTraining.Stop();
+            StartTraining((sender as Picker).SelectedItem.ToString());
+        }
+
+        private void ButtonNextStep_Clicked(object sender, EventArgs e)
+        {
+            TrainingStep step = currentTraining.NextStep();
+            if (step != null)
+            {
+                currentTraining.SyncTimer();
+                trainingStopwatch = new TimeSpan(0, 0, currentTraining.GetCurrentProgress());
+            }
+        }
+    }
+}
---
+++ Cycling Workout Trainer/Cycling Workout Trainer/Training.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace Cycling_Workout_Trainer
+{
+    public class TrainingStep
+    {
+        public int Seconds;
+        public int RPM;
+        public string Description;
+    }
+
+    public class TickEventArgs
+    {
+        public TickEventArgs(int remainingSeconds) { RemainingStepSeconds = remainingSeconds; }
+        public int RemainingStepSeconds { get; }
+    }
+
+    public class NextStepEventArgs
+    {
+        public NextStepEventArgs(TrainingStep step, TrainingStep nextStep) { NewStep = step; NextStep = nextStep; }
+        public TrainingStep NewStep { get; }
+        public TrainingStep NextStep { get; }
+    }
+
+    class Training
+    {
+        public static Training Default = new Training("Big Training")
+        {
+            trainingSteps = new TrainingStep[]
+            {
+                // Разминка
+                new TrainingStep() { Seconds = 5 * 60, RPM = 60, Description = "Режим отдыха" },
+                new TrainingStep() { Seconds = 2 * 60, RPM = 80, Description = "Круиз (2-1)" },
+                new TrainingStep() { Seconds = 3 * 60, RPM = 60, Description = "Режим отдыха" },
+                
+                // Основная часть
+                new TrainingStep() { Seconds = 8 * 60, RPM = 80, Description = "Круиз (2-1)" },
+                new TrainingStep() { Seconds = 2 * 60, RPM = 60, Description = "Режим отдыха (вода)" },
+                new TrainingStep() { Seconds = 8 * 60, RPM = 80, Description = "Круиз (2-2)" },
+                new TrainingStep() { Seconds = 2 * 60, RPM = 60, Description = "Режим отдыха (вода)" },
+                new TrainingStep() { Seconds = 5 * 60, RPM = 80, Description = "Интенсивно (2-3)" },
+                new TrainingStep() { Seconds = 5 * 60, RPM = 80, Description = "Круиз (2-1)" },
+
+                // Заминка
+                new TrainingStep() { Seconds = 5 * 60, RPM = 60, Description = "Режим отдыха" },
+            },
+        };
+
+        public static Training DefaultSmall = new Training("Small Training")
+        {
+            trainingSteps = new TrainingStep[]
+            {
+                // Разминка
+                new TrainingStep() { Seconds = 3 * 60, RPM = 60, Description = "Режим отдыха" },
+                
+                // Основная часть
+                new TrainingStep() { Seconds = 6 * 60, RPM = 80, Description = "Круиз (2-1)" },
+                new TrainingStep() { Seconds = 2 * 60, RPM = 60, Description = "Режим отдыха (вода)" },
+                new TrainingStep() { Seconds = 6 * 60, RPM = 80, Description = "Интенсивно (2-2)" },
+
+                // Заминка
+                new TrainingStep() { Seconds = 3 * 60, RPM = 60, Description = "Режим отдыха" },
+            },
+        };
+
+        public static Training Debug = new Training("Debug Training")
+        {
+            trainingSteps = new TrainingStep[]
+            {
+                new TrainingStep() { Seconds = 8, RPM = 60, Description = "Режим отдыха" },
+                new TrainingStep() { Seconds = 15, RPM = 80, Description = "Круиз (2-1)" },
+                new TrainingStep() { Seconds = 8, RPM = 60, Description = "Режим отдыха (вода)" },
+                new TrainingStep() { Seconds = 15, RPM = 80, Description = "Интенсивно (2-2)" },
+                new TrainingStep() { Seconds = 8, RPM = 60, Description = "Режим отдыха" },
+            },
+        };
+
+        public delegate void TickEventHandler(object sender, TickEventArgs e);
+        public event TickEventHandler TickEvent;
+        public delegate void NextStepEventHandler(object sender, NextStepEventArgs e);
+        public event NextStepEventHandler NextStepEvent;
+
+        public readonly string Name;
+        public int TotalSteps { get => trainingSteps.Length; }
+        public int CurrentStep { get => step + 1; }
+        public int TotalTrainingTime
+        {
+            get
+            {
+                if (totalTrainingTime == 0)
+                    foreach (var step in trainingSteps)
+                        totalTrainingTime += step.Seconds;
+
+                return totalTrainingTime;
+            }
+        }
+        int totalTrainingTime = 0;
+
+        TrainingStep[] trainingSteps;
+        int step = 0;
+        int remainingStepSeconds = 0; // countdown to 0
+
+        Timer timer = new Timer();
+
+        public Training(string name)
+        {
+            Name = name;
+        }
+
+        public TrainingStep Start()
+        {
+            if (trainingSteps == null || trainingSteps.Length == 0)
+                return null;
+
+            step = 0;
+            remainingStepSeconds = trainingSteps[0].Seconds;
+
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 1000;
+            timer.Start();
+
+            NextStepEvent?.Invoke(this, new NextStepEventArgs(trainingSteps[0], PeekNextStep()));
+            TickEvent?.Invoke(this, new TickEventArgs(remainingStepSeconds)); // TODO: remove?
+
+            return trainingSteps[0];
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            remainingStepSeconds--;
+
+            if (remainingStepSeconds <= 0)
+            {
+                TrainingStep nextStep = NextStep();
+                if (nextStep == null)
+                    (sender as Timer).Stop();
+            }
+
+            TickEvent?.Invoke(this, new TickEventArgs(remainingStepSeconds));
+        }
+
+        public int GetCurrentProgress()
+        {
+            int currentSeconds = 0; // time spent on previous steps
+
+            for (int i = 0; i < step; i++)
+                currentSeconds += trainingSteps[i].Seconds;
+
+            return currentSeconds;
+        }
+
+        public TrainingStep GetCurrentStep()
+        {
+            return trainingSteps[step];
+        }
+
+        public TrainingStep PeekNextStep()
+        {
+            if (step + 1 < trainingSteps.Length)
+                return trainingSteps[step + 1];
+
+            return null;
+        }
+
+        public TrainingStep NextStep()
+        {
+            TrainingStep nextStep = PeekNextStep();
+            if (nextStep != null)
+            {
+                remainingStepSeconds = nextStep.Seconds;
+                step++;
+                TrainingStep nextNextStep = PeekNextStep();
+                NextStepEvent?.Invoke(this, new NextStepEventArgs(nextStep, nextNextStep)); // TODO: invoke even if null to allow training finnalyzing?
+            }
+
+            return nextStep;
+        }
+
+        public void SyncTimer()
+        {
+            timer.Stop();
+            TickEvent?.Invoke(this, new TickEventArgs(remainingStepSeconds));
+            timer.Start();
+        }
+    }
+}
